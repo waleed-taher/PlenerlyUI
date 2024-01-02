8f0718e74c293ca0702f89fc0b85409d47497a93
Muhammad Waleed <waleed.taher17@gmail.com>
2024-01-02 12:57:55 +0500

    feat(renter_dashboard_layout): renter dashboard layout

---
+++ src/app/(renter)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const RenterDashboard = () => {
+  return <div>page</div>;
+};
+
+export default RenterDashboard;
---
+++ src/app/(renter)/layout.tsx
@@ -0,0 +1,7 @@
+export default async function RenterLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
